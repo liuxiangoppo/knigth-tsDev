710310b4f8b3b0ae9825cc8a57c9e1f3bde0fc0c
xiang liu <xiangliu@xiangdeMacBook-Pro.local>
2018-02-03 10:14:05 +0800

    增加了一些必要的模块,同时删除了一些不需要的东西

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,6 @@ const gulp = require("gulp");
 const ts = require("gulp-typescript");
 const sourcemaps = require("gulp-sourcemaps");
 const nodemon = require("gulp-nodemon");
-const typedoc = require("gulp-typedoc");
-const apidoc = require("gulp-apidoc");
-const zip = require("gulp-zip");
 
 const tsProject = ts.createProject("tsconfig.json");
 const packageConfig = require("./package.json");
@@ -21,21 +18,11 @@ gulp.task("ts", function () {
 });
 
 /**
- * 创建apidoc文档
- */
-gulp.task("apidoc", (done) => {
-    apidoc({
-        src: "src/controller/",
-        dest: 'docs'
-    }, done);
-});
-
-/**
  * 监听代码改动并实时编译ts
  */
 gulp.task("watch", ["ts"], function () {
     return nodemon({
-        script: "dist/bin/Startup.js",  // 服务的启动文件
+        script: "dist/app.js",  // 服务的启动文件
         watch: "src",    // 源代码目录
         tasks: ["ts"], // 在重启服务前需要执行的任务
         ext: "ts", // 监听.ts结尾的文件 必须
@@ -46,13 +33,4 @@ gulp.task("watch", ["ts"], function () {
         // 必须开启debug模式
         exec: "node --debug"
     });
-});
-
-/**
- * 打成zip包
- */
-gulp.task("zip", ["ts"], function () {
-    return gulp.src(["**/dist/**/*.*", "package.json", "project.json", '!**/node_modules/**/*.*'])
-        .pipe(zip(`${packageConfig.name}_${packageConfig.version}.zip`))
-        .pipe(gulp.dest(`${packageConfig.zipTarget}`));
 });
