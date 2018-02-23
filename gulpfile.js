cd6ff2c5f86b511dce9906602dda0de5342850fe
xiang liu <xiangliu@xiangdeMacBook-Pro.local>
2018-02-23 09:59:48 +0800

    新增打成zip以及做了部分调整

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,13 @@ const gulp = require("gulp");
 const ts = require("gulp-typescript");
 const sourcemaps = require("gulp-sourcemaps");
 const nodemon = require("gulp-nodemon");
+const zip = require("gulp-zip");
 
 const tsProject = ts.createProject("tsconfig.json");
 const packageConfig = require("./package.json");
 
 /**
- * ts编译
+ * Typescript编译
  */
 gulp.task("ts", function () {
     return tsProject.src()
@@ -22,7 +23,7 @@ gulp.task("ts", function () {
  */
 gulp.task("watch", ["ts"], function () {
     return nodemon({
-        script: "dist/app.js",  // 服务的启动文件
+        script: packageConfig.main,  // 服务的启动脚本文件,默认从package.json中获取,亦可自行设置
         watch: "src",    // 源代码目录
         tasks: ["ts"], // 在重启服务前需要执行的任务
         ext: "ts", // 监听.ts结尾的文件 必须
@@ -33,4 +34,13 @@ gulp.task("watch", ["ts"], function () {
         // 必须开启debug模式
         exec: "node --debug"
     });
+});
+
+/**
+ * 打成zip包
+ */
+gulp.task("zip", ["ts"], function () {
+    return gulp.src(["**/dist/**/*.*", "package.json", "project.json", '!**/node_modules/**/*.*'])
+        .pipe(zip(`${packageConfig.name}_${packageConfig.version}.zip`))
+        .pipe(gulp.dest(`${packageConfig.zipTarget}`));
 });
